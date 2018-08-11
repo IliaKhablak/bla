f5234d422ae66855ebbbc356f5642a895a06add8
ilia <apella2010@gmail.com>
2018-08-11 22:38:43 +0800

    wow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,38 @@
 import { Component} from '@angular/core';
 import { Angular2TokenService } from 'angular2-token';
 import {ProdService} from './services/prod.service';
+import {trigger, animate, transition, style} from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+     trigger(
+      'enterAnimation', [
+        transition('void => *', [
+          style({opacity: 1}),
+          animate('600ms', style({opacity: 1}))
+        ]),
+        transition('* => void', [
+          style({opacity: 1}),
+          animate('600ms', style({opacity: 0}))
+        ])
+      ]
+     )]
 })
 export class AppComponent {
+
+	bla:boolean = false;
 
 	constructor(private tokenService:Angular2TokenService, public prodService:ProdService){
 		this.tokenService.init ({
 		    apiBase: 'https://infinite-reaches-26736.herokuapp.com/'
 		});
+		window.setTimeout(() => {
+			this.bla = true;
+		}, 5000)
 	}
 
 	
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import {ProdService} from '../services/prod.service';
 import * as $ from 'jquery';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -25,6 +26,18 @@ export class HomeComponent implements OnInit {
   securityDiv:any;
   hexOverl:any;
   overlHexTop:any;
+  firstPTop:number;
+  secondPTop:number;
+  mouseOvered:boolean;
+  mouseOvered3:boolean;
+  mouseOvered2:boolean;
+  mouseOvered4:boolean;  
+  pchanged2:boolean;
+  pchanged:boolean;
+  buttonFixed:number;
+  buttonFixed2:number;
+  buttonFixed3:any;
+  status:boolean = false;
   imgs:string[]=[
     "/assets/images/1.jpg",
     "/assets/images/1.jpg",
@@ -33,7 +46,7 @@ export class HomeComponent implements OnInit {
     "/assets/images/1.jpg"
   ];
 
-  constructor(private prod:ProdService) {
+  constructor(private prod:ProdService, private router:Router) {
   	// document.getElementById('parallax').scrollTo(0,0);
 
   	
@@ -42,33 +55,44 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.prod.btntoggle = false;
     this.prod.nav = false;
-    this.wow = document.getElementById('parallax-try-id');
-    this.bla3 = document.getElementById('parallax-try-id3');
-    let a = document.getElementById('parallax-try-id2');
-    let b = a.offsetTop;
-    let c = a.offsetHeight;
+    this.wow = $('#parallax-try-id');
+    this.bla3 = $('#parallax-try-id3');
+    let a = $('#parallax-try-id2');
+    let b = a.offset().top;
+    let c = a.height();
     this.wow1 = b+c-this.wow2;
-    let q = document.getElementById('parallax-try-id4');
-    let w = q.offsetTop;
-    let e = q.offsetHeight;
+    let q = $('#parallax-try-id4');
+    let w = q.offset().top;
+    let e = q.height();
     this.bla1 = w+e-this.wow2;
-    this.disapW = document.getElementById('disapW');
-    this.disapP = document.getElementById('disapP');
-    this.disapS = document.getElementById('disapS');
+    this.disapW = $('#disapW');
+    this.disapP = $('#disapP');
+    this.disapS = $('#disapS');
     this.disapPTop = w+this.wow2+200;
-    let r = document.getElementById('designDiv');
-    let t = r.offsetTop;
-    let y = r.offsetHeight;
+    let r = $('#designDiv');
+    let t = r.offset().top;
+    let y = r.height();
     this.designDiv = t+y-this.wow2;
-    this.designDivOverlay = document.getElementById('parallax-try-id5');
-    let u = document.getElementById('securityDiv');
-    let i = u.offsetTop;
-    let o = u.offsetHeight;
+    this.designDivOverlay = $('#parallax-try-id5');
+    let u = $('#securityDiv');
+    let i = u.offset().top;
+    let o = u.height();
     this.securityDiv = t+y+this.wow2;
-    this.hexOverl = document.getElementById('hexoverl');
+    this.hexOverl = $('#hexoverl');
     this.overlHexTop = i+o-this.wow2;
+    this.firstPTop = $('#firstPTop').offset().top;
+    this.secondPTop = $('#secondPTop').offset().top;
+    // console.log("LOOK HERE!",this.bla1+this.wow2);
+    this.buttonFixed = t;
+    this.buttonFixed2 = t+y-this.wow2-100;
+    this.buttonFixed3 = $('#contactMeButton');
     // console.log("POSITION HERE!",this.wow1);
 
+  }
+
+  doSomething(){
+    // console.log(this.status);
+    this.status = !this.status;
   }
 
   ngOnDestroy(){
@@ -83,6 +107,11 @@ export class HomeComponent implements OnInit {
     document.getElementById(val).classList.remove("changed");
   }
 
+  sendMeThere(str:string){
+    console.log(str);
+    window.location.href = str;
+  }
+
   @HostListener ('window:scroll' ,['$event'])
    onWindowScroll() {
     const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
@@ -93,50 +122,57 @@ export class HomeComponent implements OnInit {
       }
       this.scrollPos = offset;
       // console.log(this.scrollPos)
+
       if(this.scrollPos>this.wow1){
         let a = 1-(((this.scrollPos - this.wow1)/1000)*2);
         let b = a.toFixed(2);
-
-        // console.log(b);
-        this.wow.style = "background: rgba(0, 0, 0,"+b+");";
+        this.wow.css("background","rgba(0, 0, 0,"+b+")");
       }
       if(this.scrollPos>this.bla1){
         let a = 1-(((this.scrollPos - this.bla1)/1000)*2);
         let b = a.toFixed(2);
-
-        // console.log(b);
-        this.bla3.style = "background: rgba(0, 0, 0,"+b+");";
+        this.bla3.css("background","rgba(0, 0, 0,"+b+")");
       }
-      if(this.scrollPos>this.bla1+this.wow2-100){this.bla3.style = "display:none;"}
+      if(this.scrollPos>this.bla1+this.wow2){this.bla3.css("display","none")}else{this.bla3.css("display","block")}
       if(this.scrollPos>200){
         let a = 1-(((this.scrollPos - 200)/1000)*2);
         let b = a.toFixed(2);
-        this.disapW.style = "opacity:"+b+";";
-      }else{this.disapW.style = "opacity:1;";}
+        this.disapW.css("opacity",b);
+      }else{this.disapW.css("opacity",1)}
       if(this.scrollPos>this.disapPTop){
         let a = 1-(((this.scrollPos - this.disapPTop)/1000)*2);
         let b = a.toFixed(2);
-        this.disapP.style = "opacity:"+b+";";
-      }else{this.disapP.style = "opacity:1;";}
+        this.disapP.css("opacity",b);
+      }else{this.disapP.css("opacity",1)}
       if(this.scrollPos>this.designDiv){
         let a = 1-(((this.scrollPos - this.designDiv)/1000)*2);
         let b = a.toFixed(2);
-
-        // console.log(b);
-        this.designDivOverlay.style = "background: rgba(0, 0, 0,"+b+");";
+        this.designDivOverlay.css("background","rgba(0, 0, 0,"+b+")");
       }
       if(this.scrollPos>this.securityDiv){
         let a = 1-(((this.scrollPos - this.securityDiv)/1000)*2);
         let b = a.toFixed(2);
-        this.disapS.style = "opacity:"+b+";";
-      }else{this.disapS.style = "opacity:1;";}
+        this.disapS.css("opacity",b);
+      }else{this.disapS.css("opacity",1)}
       if(this.scrollPos>this.overlHexTop){
         let a = 1-(((this.scrollPos - this.overlHexTop)/1000)*2);
         let b = a.toFixed(2);
-
-        // console.log(b);
-        this.hexOverl.style = "background: rgba(0, 0, 0,"+b+");";
+        this.hexOverl.css("background","rgba(0, 0, 0,"+b+")");
       }
+      if(this.scrollPos>this.overlHexTop+this.wow2){this.hexOverl.css("display","none")}else{this.hexOverl.css("display","block")}
+      if(this.scrollPos>this.firstPTop){
+        $('.firstP').addClass("firstPChanged");
+      }else{
+        $('.firstP').removeClass("firstPChanged");
+      }
+      if(this.scrollPos>this.secondPTop){
+        $('.secondP').addClass("firstPChanged");
+      }else{
+        $('.secondP').removeClass("firstPChanged");
+      }
+      if(this.scrollPos>this.buttonFixed && this.scrollPos<this.buttonFixed2){
+        this.buttonFixed3.addClass("changeColor");
+      }else{this.buttonFixed3.removeClass("changeColor")}
   }
 
 }
